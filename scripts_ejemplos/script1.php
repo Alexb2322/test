b0d5bd0598bb36d2e56a51d679ea5b0b181a7dce
Alex <axel2322@gmail.com>
2019-03-14 21:57:48 -0500

    mi primer script

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,18 @@
+<center>MI PRIMER SCRIPT</center>
+<?php
+    //Mi primer script en PHP
+    //Desarrollado por: Alex
+    $nombre="Alex";
+    $apellido="Basante";
+    $email="axel2322@gmail.com";
+
+    echo "<center>html en php</center>";
+    echo "<center>";
+    echo "<center>Bienvenido: ".$nombre." ".$apellido."<br></center>"; 
+    echo "</center>";
+    echo $nombre."<br>"; // . concatena
+    echo $apellido."<br>";
+    echo $email."<br>";
+    
+?>
+<center>FIN DEL SCRIPT</center>
