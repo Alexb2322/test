d4de6807916033c77959c6847bc725130fac1044
Alex <axel2322@gmail.com>
2019-03-15 20:06:13 -0500

    calculadora2

---
+++ scripts_ejemplos/calculadora.php
@@ -0,0 +1,21 @@
+<?php
+$num1=$_POST['n1'];
+$num2=$_POST['n2'];
+$oper=$_POST['tipo_op'];
+
+echo "Numero 1: ". $num1."<br>";
+echo "Numero 2: ". $num2."<br>";
+echo "Tipo operacion: " .$oper;
+
+if($oper=="1")
+    $resul=$num1+$num2;
+    else
+        if($oper=="2")
+        $resul=$num1-$num2;
+     else
+        if($oper=="3")
+        $resul=$num1*$num2;
+    else
+        $resul=$num1/$num2;
+echo "Resultado es: " .$resul            
+?>
