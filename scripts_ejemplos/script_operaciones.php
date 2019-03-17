046e7165f5a6cb08da8ff452eebee386b223bda1
Alex <axel2322@gmail.com>
2019-03-17 15:04:45 -0500

    script operaciones

---
+++ scripts_ejemplos/script_operaciones.php
@@ -0,0 +1,17 @@
+<center>SCRIPT OPERACIONES<br><br><center>
+<?php
+$n1=25;
+$n2=5;
+
+$suma=$n1+$n2;
+$resta=$n1-$n2;
+$multi=$n1*$n2;
+$divi=$n1/$n2;
+
+echo "<center>Los numeros son: ".$n1." y ".$n2."<br><br></center>";
+echo "<center>La Suma es : ".$suma."<br></center>";
+echo "<center>La Resta es : ".$resta."<br></center>";
+echo "<center>La Multiplicacion es : ".$multi."<br></center>";
+echo "<center>La Division es : ".$divi."<br></center>";
+
+?>
